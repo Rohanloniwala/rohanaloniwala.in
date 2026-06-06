170cb9877b22e331735274565de60beeb212fa2b
Rohanloniwala <seh.rohan@gmail.com>
2026-06-06 14:12:45 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
+import PreWeddingOffers from './PreWeddingOffers.jsx'
+import SongShootOffers from './SongShootOffers.jsx'
+
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/prewedding" element={<PreWeddingOffers />} />
+        <Route path="/songshoot" element={<SongShootOffers />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
